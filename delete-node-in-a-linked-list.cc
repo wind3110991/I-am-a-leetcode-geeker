c890f3d737f0fe6f0e93a29e82c2c97034d76b55
Wind.Young <312456073@qq.com>
2020-03-11 13:10:19 +0800

    Update delete-node-in-a-linked-list.cc

--- delete-node-in-a-linked-list.cc
+++ delete-node-in-a-linked-list.cc
@@ -16,8 +16,3 @@ public:
         delete delNode;
     }
 };
-
-作者：windyoung
-链接：https://leetcode-cn.com/problems/delete-node-in-a-linked-list/solution/shen-qi-de-yi-ti-yi-kai-shi-du-meng-liao-by-windyo/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
